71503b015cc98f68f8757445857688001389887c
Wenhua-Zheng <125440482+Wenhua-Zheng@users.noreply.github.com>
2024-03-04 23:49:43 +0800

    Delete simulation experiments directory

--- simulation experiments/In.m
+++
@@ -1,6 +0,0 @@
-% intersection
-function s=In(A,B)
-s.t=max(A.t,B.t);
-s.b=min(A.b,B.b);
-s.l=max(A.l,B.l);
-s.r=min(A.r,B.r);
--- simulation experiments/getColor.m
+++
@@ -1,8 +0,0 @@
-%  Color supplement
-function color = getColor(idx,num)
-    count =fix(num/3) + 1;
-    g_count = fix((idx+2)/3);
-    b_count = fix((idx+1)/3);
-    r_count = fix(idx/3);
-    color = [1-g_count/count,1-b_count/count,1-r_count/count];
-end
